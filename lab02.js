f6f53cc6273785a50020b5597051d711ab1daebf
Camila Richter <ric23183@uvg.edu.gt>
2026-02-13 10:10:08 -0600

    query ejercicio 2.2 b y c

--- lab02.js
+++ lab02.js
@@ -95,3 +95,84 @@ db.usuarios.find(
     cantidad_amigos: 1
   }
 ).explain("executionStats")
+
+//1.4
+
+//1.5
+
+//1.6
+
+//1.7
+
+//2.1
+
+//2.2a
+
+//2.2b
+db.vehiculos.updateMany(
+  {},
+  [
+    {
+      $set: {
+        price: {
+          $toDouble: {
+            $substrCP: ["$price", 1, { $strLenCP: "$price" }]
+          }
+        }
+      }
+    }
+  ]
+)
+
+db.vehiculos.aggregate([
+  { $project: { price: 1, tipo: { $type: "$price" } } }
+])
+
+db.vehiculos.aggregate([
+  {
+    $group: {
+      _id: {
+        brand: "$brand",
+        year: "$year"
+      },
+      cantidad_modelos: { $sum: 1 },
+      precio_promedio: { $avg: "$price" }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      brand: "$_id.brand",
+      year: "$_id.year",
+      cantidad_modelos: 1,
+      precio_promedio: { $round: ["$precio_promedio", 2] }
+    }
+  },
+  {
+    $sort: { brand: 1, year: 1 }
+  }
+]);
+
+//2.2c
+db.carros.aggregate([
+  {
+    $match: {
+      year: { $gte: 1990, $lte: 1999 }
+    }
+  },
+  {
+    $sort: { price: -1 }
+  },
+  {
+    $limit: 20
+  },
+  {
+    $project: {
+      _id: 0,
+      brand: 1,
+      model: 1,
+      year: 1,
+      price: 1
+    }
+  }
+])

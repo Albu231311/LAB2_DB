d22f153bef08b9a4cb2a74adf43ec7d21b1bc2bd
Camila Richter <ric23183@uvg.edu.gt>
2026-02-13 09:54:33 -0600

    ejercicio 1.3, descripción del rendimiento

--- lab02.js
+++ lab02.js
@@ -46,3 +46,52 @@ db.usuarios.find(
     cantidad_amigos: 1
   }
 )
+
+//sección 1.3
+db.usuarios.find(
+  {
+    activo: true,
+    puntos: { $gt: 500 }},
+  {
+    _id: 0,
+    nombre: 1,
+    activo: 1,
+    puntos:1,
+  }
+). sort({ puntos: -1 }).explain("executionStats")
+
+db.usuarios.find(
+  {
+    "historial_compras.producto": "Producto 1"
+  },
+  {
+    _id: 0,
+    nombre: 1,
+    "historial_compras.producto": 1,
+  }
+).explain("executionStats")
+
+db.usuarios.find(
+  {
+    tags: "tag2",
+    visitas: { $gt: 100 }},
+  {
+    _id: 0,
+    nombre: 1,
+    tags: 1,
+    visitas:1,
+  }
+). sort({ vistas: -1 }).explain("executionStats")
+
+db.usuarios.find(
+  {
+    "preferencias.color": "azul",
+    cantidad_amigos: { $gte: 1000, $lte: 2000 }
+  },
+  {
+    _id: 0,
+    nombre: 1,
+    "preferencias.color": 1,
+    cantidad_amigos: 1
+  }
+).explain("executionStats")
